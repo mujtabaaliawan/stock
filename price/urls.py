8820dd5da352e76009f9184797993136b8ffeba1
mujtabaaliawan <117171332+mujtabaaliawan@users.noreply.github.com>
2022-12-08 11:19:57 +0500

    a

--- price/urls.py
+++ price/urls.py
@@ -3,6 +3,5 @@ from price import views
 
 
 urlpatterns = [
-    path('prices', views.PriceList.as_view(), name='price_list'),
     path('latest', views.LatestPriceList.as_view(), name='market_price')
 ]
