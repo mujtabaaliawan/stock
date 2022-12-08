101ec3ee32683c000d2f991f0d01f83b2f078813
mujtabaaliawan <117171332+mujtabaaliawan@users.noreply.github.com>
2022-12-08 18:24:29 +0500

    a

--- company/models.py
+++ company/models.py
@@ -5,7 +5,7 @@ from category.models import Category
 class Company(models.Model):
 
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='company_category')
 
     class Meta:
         ordering = ['id']
--- company/views.py
+++ company/views.py
@@ -9,14 +9,17 @@ from price.models import Price
 
 
 class CompanyList(ListAPIView):
+    
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
 
 class GraphLogger(APIView):
+
     parser_classes = [JSONParser]
 
     def post(self, request):
+
         from_datetime_value = request.data['from_datetime']
         from_datetime = datetime.strptime(from_datetime_value, '%Y-%m-%d %H:%M:%S')
         to_datetime_value = request.data['to_datetime']
@@ -35,7 +38,6 @@ class GraphLogger(APIView):
         company_data['company_name'] = company_name
 
         price_data = list(company_prices.values())
-
         graph_data = [company_data, price_data]
 
         return JsonResponse(graph_data, safe=False)
