6f220fcefc9a4ad694747ab465c0706ada9ef61c
mujtabaaliawan <117171332+mujtabaaliawan@users.noreply.github.com>
2022-12-19 18:35:19 +0500

    docker-compose updated

--- stock/settings.py
+++ stock/settings.py
@@ -81,21 +81,21 @@ TEMPLATES = [
 WSGI_APPLICATION = env('WSGI_APPLICATION')
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': BASE_DIR / 'db.sqlite3',
+        # }
+    'default': {
+        'ENGINE': env('DATABASE_ENGINE'),
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+        'TEST': {
+            'NAME': env('DATABASE_TEST_NAME'),
         }
-    # 'default': {
-    #     'ENGINE': env('DATABASE_ENGINE'),
-    #     'NAME': env('DATABASE_NAME'),
-    #     'USER': env('DATABASE_USER'),
-    #     'PASSWORD': env('DATABASE_PASSWORD'),
-    #     'HOST': env('DATABASE_HOST'),
-    #     'PORT': env('DATABASE_PORT'),
-    #     'TEST': {
-    #         'NAME': env('DATABASE_TEST_NAME'),
-    #     }
-    # }
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
